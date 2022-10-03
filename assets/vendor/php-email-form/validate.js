f0c216612dcdbfc5ce549ab767cbb1a562a81c93
expertdigitals <111439005+expertdigitals@users.noreply.github.com>
2022-10-03 22:02:14 +0300

    Website V1.1.4

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -72,10 +72,4 @@
     });
   }
 
-  function displayError(thisForm, error) {
-    thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
-    thisForm.querySelector('.error-message').classList.add('d-block');
-  }
-
 })();
